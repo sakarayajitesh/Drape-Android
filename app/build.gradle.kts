41106965f58815a6f0422707a78c0edaa3ddf89c
Ajitesh Sakaray <sakarayajitesh@gmail.com>
2024-03-11 14:19:30 +0530

    release: 1.0.9 (10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ajitesh.drape"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.0.8"
+        versionCode = 10
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
